68790bcb8298aa5fa8e005f616197777424662fd
hammerc <hammerc@163.com>
2015-06-20 11:42:55 +0800

    添加2D相关的编辑器扩展脚本。

---
+++ Editor/2D/MakeSpritePrefabsScript.cs
@@ -0,0 +1,94 @@
+﻿// =================================================================================================
+//
+//	Hammerc Library
+//	Copyright 2015 hammerc.org All Rights Reserved.
+//
+//	See LICENSE for full license information.
+//
+// =================================================================================================
+
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// 2D 图集生成对应预制件脚本类.
+/// 
+/// 解决问题: 
+/// 解决不能动态设置 Sprite 对象的贴图的问题, 因为分散的小图最终需要打包为图集而 Unity3D 又将图集的处理隐藏, 所以需要将小图的信息存放到预制件中.
+/// 
+/// 使用方法及步骤: 
+/// 1.修改源目录及目标目录为自己使用的目录;
+/// 2.把小图存放到源目录中;
+/// 3.在菜单栏点击 Hammerc/2D/MakeSpritePrefabs 即可在目标目录下生成对应的预制件;
+/// 4.通过获取生成的预制件的 sprite 对象即可在程序中使用小图.
+/// 
+/// </summary>
+public class MakeSpritePrefabsScript
+{
+    /// <summary>
+    /// Assets 目录下的小图片目录, 包括子目录的所有图片文件都会进行处理.
+    /// </summary>
+    private const string ORIGIN_DIR = "\\RawData\\Sprites";
+
+    /// <summary>
+    /// Assets 目录下的小图预制件生成的目标目录, 注意该目录下不要存放其他资源, 每次生成时都会清空该目录下的所有文件.
+    /// </summary>
+    private const string TARGET_DIR = "\\Resources\\Sprites";
+
+    /// <summary>
+    /// 将制定目录下的原始图片一对一打包成 Prefab 方便在游戏运行中读取指定的图片.
+    /// </summary>
+    [MenuItem("Hammerc/2D/MakeSpritePrefabs")]
+    private static void MakeSpritePrefabs()
+    {
+        EditorUtility.DisplayProgressBar("Make Sprite Prefabs", "Please wait...", 1);
+
+        string targetDir = Application.dataPath + TARGET_DIR;
+        //删除目标目录
+        if(Directory.Exists(targetDir))
+            Directory.Delete(targetDir, true);
+        if(File.Exists(targetDir + ".meta"))
+            File.Delete(targetDir + ".meta");
+        //创建空的目标目录
+        if(!Directory.Exists(targetDir))
+            Directory.CreateDirectory(targetDir);
+
+        //获取源目录的所有图片资源并处理
+        string originDir = Application.dataPath + ORIGIN_DIR;
+        DirectoryInfo originDirInfo = new DirectoryInfo(originDir);
+        MakeSpritePrefabsProcess(originDirInfo.GetFiles("*.jpg", SearchOption.AllDirectories), targetDir);
+        MakeSpritePrefabsProcess(originDirInfo.GetFiles("*.png", SearchOption.AllDirectories), targetDir);
+
+        EditorUtility.ClearProgressBar();
+    }
+
+    static private void MakeSpritePrefabsProcess(FileInfo[] files, string targetDir)
+    {
+        foreach(FileInfo file in files)
+        {
+            string allPath = file.FullName;
+            string assetPath = allPath.Substring(allPath.IndexOf("Assets"));
+            //加载贴图
+            Sprite sprite = AssetDatabase.LoadAssetAtPath<Sprite>(assetPath);
+            //创建绑定了贴图的 GameObject 对象
+            GameObject go = new GameObject(sprite.name);
+            go.AddComponent<SpriteRenderer>().sprite = sprite;
+            //获取目标路径
+            string targetPath = assetPath.Replace("Assets" + ORIGIN_DIR + "\\", "");
+            //去掉后缀
+            targetPath = targetPath.Substring(0, targetPath.IndexOf("."));
+            //得到最终路径
+            targetPath = targetDir + "\\" + targetPath + ".prefab";
+            //得到应用当前目录的路径
+            string prefabPath = targetPath.Substring(targetPath.IndexOf("Assets"));
+            //创建目录
+            Directory.CreateDirectory(prefabPath.Substring(0, prefabPath.LastIndexOf("\\")));
+            //生成预制件
+            PrefabUtility.CreatePrefab(prefabPath.Replace("\\", "/"), go);
+            //销毁对象
+            GameObject.DestroyImmediate(go);
+        }
+    }
+}
---
+++ Editor/2D/SpriteSheetSlicer.cs
@@ -0,0 +1,72 @@
+﻿// =================================================================================================
+//
+//	Hammerc Library
+//	Copyright 2015 hammerc.org All Rights Reserved.
+//
+//	See LICENSE for full license information.
+//
+// =================================================================================================
+
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// 2D 图集切割类, 使用 U3D 内置的切割功能将合成好的大图切割为小图.
+/// 
+/// 解决问题: 
+/// 从网上下载或破解得到的已经导出的图集, 可以用这个脚本将其重新分割.
+/// 
+/// 使用方法及步骤: 
+/// 1.先把要切割的图集导入 Unity3D 中;
+/// 2.选择图片, 在属性面板里将纹理类型更改为 Advanced , 将 Read/Write Enabled 属性打勾, 将 Sprite Mode 改成 Multiple, 去掉 Mipmap 生成;
+/// 3.点击 Apply 按钮进行应用;
+/// 4.点击 Sprite Editor 按钮打开编辑界面对图集进行分割和对小图命名, 完成后点击 Apply 按钮应用分割;
+/// 5.选中图集, 在菜单栏点击 Hammerc/2D/SpriteSheetSplit 即可在图集同一目录下生成同名的文件夹, 文件夹中会按命名导出所有的小图.
+/// 
+/// </summary>
+public class SpriteSheetSlicer
+{
+    /// <summary>
+    /// 将选定的带有分割信息的图片切割为同目录下的多个小图.
+    /// </summary>
+    [MenuItem("Hammerc/2D/SpriteSheetSplit")]
+    private static void SpriteSheetSplit()
+    {
+        EditorUtility.DisplayProgressBar("Make Sprite Slices", "Please wait...", 1);
+
+        //获取选择的对象
+        Texture2D selectionTexture2D = Selection.activeObject as Texture2D;
+        if(selectionTexture2D == null)
+        {
+            return;
+        }
+        //获取文件夹路径
+        string rootPath = Path.GetDirectoryName(AssetDatabase.GetAssetPath(selectionTexture2D));
+        //获取图片路径名称
+        string path = rootPath + "/" + selectionTexture2D.name + ".png";
+        //获取纹理导入对象
+        TextureImporter textureImporter = AssetImporter.GetAtPath(path) as TextureImporter;
+        if(textureImporter == null)
+        {
+            return;
+        }
+        //创建文件夹
+        AssetDatabase.CreateFolder(rootPath, selectionTexture2D.name);
+        //遍历小图集
+        foreach(SpriteMetaData metaData in textureImporter.spritesheet)
+        {
+            //创建新的纹理对象
+            Texture2D texture2D = new Texture2D((int)metaData.rect.width, (int)metaData.rect.height);
+            //拷贝像素
+            texture2D.SetPixels(selectionTexture2D.GetPixels((int)metaData.rect.x, (int)metaData.rect.y, (int)metaData.rect.width, (int)metaData.rect.height));
+            //编码为 png 文件格式
+            var pngData = texture2D.EncodeToPNG();
+            //输出图片
+            File.WriteAllBytes(rootPath + "/" + selectionTexture2D.name + "/" + metaData.name + ".png", pngData);
+        }
+
+        EditorUtility.ClearProgressBar();
+    }
+}
